35ab0b8d554754ae229ac945b06b988725d29467
Ricardo-Ke <ricardo.kefan@gmail.com>
2018-07-20 19:45:18 +0800

    Update: url port changed

--- src/app/model/config.ts
+++ src/app/model/config.ts
@@ -3,6 +3,6 @@
  */
 
 export class Config{
-    public static baseUrl = 'http://localhost:8090/example/v1/';
+    public static baseUrl = 'http://localhost:9090/example/v1/';
     public static defaultProject = 'PXD000021';
 }
