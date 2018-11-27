0961e51a58ad953e152c8416cab5ea8e0c2e3255
baimingze <baimingze@gmail.com>
2018-11-27 11:32:13 +0800

    change the url

--- src/app/model/config.ts
+++ src/app/model/config.ts
@@ -4,7 +4,7 @@
 
 export class Config{
 //public static baseUrl = 'http://localhost:9090/example/v1/';
-    public static baseUrl = 'http://namenode:8099/example/v1/';
+    public static baseUrl = 'http://enhancer.ncpsb.org:8090/example/v1/';
     public static defaultProject = 'PXD000021';
     public static defaultMinClusterSize = 10;
 }
